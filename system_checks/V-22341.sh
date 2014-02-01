55c32a1739ff638a0289d6cf543945d2bae6ebfe
STIG Development Account <onedev@localhost.localdomain>
2014-02-01 17:29:35 -0500

    Added fix code to V-22341

--- system_checks/V-22341.sh
+++ system_checks/V-22341.sh
@@ -14,10 +14,14 @@ readonly finding_group="FUTURE_STUB"
 
 evaluate() {
   is_owned_by_root=$(ls -l /etc/gshadow | awk '{ print $3 }')
-  if [ $is_owned_by_root == "root" ]; then
-    finding_result="Not a Finding"
-  else
+  if [ $is_owned_by_root != "root" ]; then
     finding_result="Open"
+    if [ ${FIX} == "true" ]; then
+      $(chown root /etc/gshadow)
+      finding_result="Fixed"
+    fi
+  else
+    finding_result="Not a Finding"
   fi
 } 
 
